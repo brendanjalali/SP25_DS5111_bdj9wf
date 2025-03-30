dbbe72c2eea369cc8b0d5473ba5afcac57737fad
brendanjalali <bdj9wf@virginia.edu>
2025-03-30 22:15:03 +0000

    for viewing while working on yahoo.py

--- bin/gainers/wsj.py
+++ bin/gainers/wsj.py
@@ -9,15 +9,38 @@ class GainerDownloadWSJ(GainerDownload):
 
 	def download(self):
 		print("Downloading WSJ gainers")
+		raw = pd.read_html('wjsgainers.html')
+		raw[0].to_csv('wjsgainers.csv')
+		return raw
 
 
 class GainerProcessWSJ(GainerProcess):
 	def __init__(self):
-		pass
+		self.csv = 'wsjgainers.csv'
 
 	def normalize(self):
 		print("Normalizing WSJ gainers")
+		df = pd.read_csv(self.csv)
+		assert not df.empty, f"Error: {input_file} is empty."
+		if 'Unnamed: 0' in df.columns and 'Last' in df.columns and 'Chg' in df.columns:
+			df = df[['Unnamed: 0', 'Last', 'Chg', '% Chg']].copy()
+			df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
+			df['price'] = df['price'].astype(float)
+			df['price_change'] = df['price_change'].astype(float)
+			df['price_percent_change'] = df['price_percent_change'].astype(float)
+			assert not df['price'].isnull().all(), "Error: 'price' column is empty."
+		else:
+			print(f"Error: Unrecognized format in {input_file}")
+			return
+		assert not df.empty, f"Error: Processed dataframe for {input_file} is empty before saving."
+		output_file = input_file.replace('.csv', '_norm.csv')
+		df.to_csv(output_file, index=False)
+		print(f"Normalized file saved: {output_file}")
+		return df
+
 
 	def save_with_timestamp(self):
+		time = pd.to_datetime('now').strftime("%Y%m%d_%H%M%S")
+		wsjgainers_norm = self.normalize()
+		wsjgainers_norm.to_csv(f'wsjgainer_norm{time}.csv')
 		print("Saving WSJ gainers")
-
